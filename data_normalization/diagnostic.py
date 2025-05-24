32ac4644082591d97a8b703d633ddfa98ac8408b
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2025-05-24 22:30:13 +0200

    delete unneeded file that was not a part of the project

--- data_normalization/diagnostic.py
+++
@@ -1,6 +0,0 @@
-print(f"The value of __name__ is: {__name__}")
-
-if __name__ == "__main__":
-    print("This file was run directly!")
-else:
-    print("This file was imported by another program!")
