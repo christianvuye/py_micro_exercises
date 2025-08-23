70712be4332d586991990843526f9330d50f1656
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2025-08-23 09:18:28 +0200

    refactor: Format map_islongerthanfourchars.py
    This commit applies formatting changes to the map_islongerthanfourchars.py file to improve readability.

--- data_normalization/fp_concepts/map/map_islongerthanfourchars.py
+++ data_normalization/fp_concepts/map/map_islongerthanfourchars.py
@@ -1,5 +1,7 @@
 # type: ignore
 
-islongerthanfourchars = map(lambda x: True if len(x) > 4 else False, ["cat", "elephant", "dog"])
+islongerthanfourchars = map(
+    lambda x: True if len(x) > 4 else False, ["cat", "elephant", "dog"]
+)
 
 print(list(islongerthanfourchars))
